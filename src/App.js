11c72695715e4184c17b4ebc0a2d3e6bdd0afd53
Alassane Fofana <74676173+Las07@users.noreply.github.com>
2022-03-30 18:04:06 +0200

    update fetch

--- src/App.js
+++ src/App.js
@@ -97,7 +97,7 @@ class App extends Component {
       .then(response => {
         console.log('hi', response)
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://whispering-fortress-07071.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
